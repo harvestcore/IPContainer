5e82c026c9c85cadd31ee19cca5b5c3702b405c2
harvestcore <aagomezies@gmail.com>
2018-10-05 23:24:27 +0200

    Motor flasks básico. Continues #9

---
+++ src/engine.py
@@ -0,0 +1,12 @@
+import json
+from flask import Flask, jsonify
+#from ipcontainer import IPContainer
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "works :-)"
+
+if __name__ == "__main__":
+    app.run()

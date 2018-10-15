2e9cae49c14b3b8f7ce8930bacb6cf4ebd9b70d0
Ángel Gómez <aagomezies@gmail.com>
2018-10-15 12:59:38 +0200

    Mejoro motor flask. Continues #9

--- app.py
+++ app.py
@@ -11,55 +11,83 @@ def hello():
 
 @app.route("/addUser/<user>")
 def addUser(user):
-    return ipc.addUser(user)
+    if ipc.addUser(str(user)):
+        return jsonify(success=True)
+    else:
+        return jsonify(success=False)
 
 @app.route("/removeUser/<user>")
 def removeUser():
-    return ipc.removeUser(user)
+    if ipc.removeUser(user):
+        return jsonify(success=True)
+    else:
+        return jsonify(success=False)
 
-@app.route("/getNumberOfUsers")
+@app.route("/getNumberOfUsers", methods=['GET'])
 def getNumberOfUsers():
-    return ipc.getNumberOfUsers()
+    return jsonify(users=ipc.getNumberOfUsers())
 
-@app.route("/getNumberOfNetworks")
+@app.route("/getNumberOfNetworks", methods=['GET'])
 def getNumberOfNetworks():
-    return ipc.getNumberOfNetworks()
+    return jsonify(networks=ipc.getNumberOfNetworks())
 
 @app.route("/existNetwork/<user>/<_type>")
 def existNetwork(user, _type):
-    return ipc.existNetwork(user, _type)
+    if ipc.existNetwork(str(user), str(_type)):
+        return jsonify(exists=True)
+    else:
+        return jsonify(exists=False)
 
 @app.route("/createNetwork/<user>/<_type>")
 def createNetwork(user, _type):
-    return ipc.createNetwork(user, _type)
+    if ipc.createNetwork(str(user), str(_type)):
+        return jsonify(success=True)
+    else:
+        return jsonify(success=False)
 
 @app.route("/removeNetwork/<user>/<_type>")
 def removeNetwork(user, _type):
-    return ipc.removeNetwork(user, _type)
+    if ipc.removeNetwork(str(user), str(_type)):
+        return jsonify(success=True)
+    else:
+        return jsonify(success=False)
 
 @app.route("/addIPtoNetwork/<user>/<_type>/<ip>")
 def addIPtoNetwork(user, _type, ip):
-    return ipc.addIPtoNetwork(user, _type, ip)
+    if ipc.addIPtoNetwork(str(user), str(_type), str(ip)):
+        return jsonify(success=True)
+    else:
+        return jsonify(success=False)
+    
 
 @app.route("/removeIPfromNetwork/<user>/<_type>/<ip>")
 def removeIPfromNetwork(user, _type, ip):
-    return ipc.removeIPfromNetwork(user, _type, ip)
+    if ipc.removeIPfromNetwork(str(user), str(_type), str(ip)):
+        return jsonify(success=True)
+    else:
+        return jsonify(success=False)
 
 @app.route("/getNetworkSize/<user>/<_type>")
 def getNetworkSize(user, _type):
-    return ipc.getNetworkSize(user, _type)
+    return jsonify(user=user, network=_type, size=ipc.getNetworkSize(str(user), str(_type)))
 
 @app.route("/getData/<user>/<_type>")
 def getData(user, _type):
-    return ipc.getData(user, _type)
+    return jsonify(user=user, network=_type, data=ipc.getData(str(user), str(_type)))
 
 @app.route("/_dropUsers")
 def _dropUsers():
-    return ipc._dropUsers()
+    if ipc._dropUsers():
+        return jsonify(success=True)
+    else:
+        return jsonify(success=False)
 
 @app.route("/_dropData")
 def _dropData():
-    return ipc._dropData()
+    if ipc._dropData():
+        return jsonify(success=True)
+    else:
+        return jsonify(success=False)
 
 if __name__ == "__main__":
     app.run()

3431d56c769617fd18947fbef4e148f5f8bea762
Ángel Gómez <aagomezies@gmail.com>
2018-10-17 12:34:12 +0200

    Limpio código. Continues #9

--- app.py
+++ app.py
@@ -3,91 +3,66 @@ from flask import Flask, jsonify
 from src.ipcontainer import IPContainer
 
 app = Flask(__name__)
-ipc = IPContainer()
 
 @app.route("/")
 def hello():
-    return "works :-)"
+    return "who dis"
 
 @app.route("/addUser/<user>")
 def addUser(user):
-    if ipc.addUser(str(user)):
-        return jsonify(success=True)
-    else:
-        return jsonify(success=False)
+    return jsonify(success=IPContainer.addUser(str(user)))
 
 @app.route("/removeUser/<user>")
-def removeUser():
-    if ipc.removeUser(user):
-        return jsonify(success=True)
-    else:
-        return jsonify(success=False)
+def removeUser(user):
+    return jsonify(success=IPContainer.removeUser(str(user)))
 
 @app.route("/getNumberOfUsers", methods=['GET'])
 def getNumberOfUsers():
-    return jsonify(users=ipc.getNumberOfUsers())
+    return jsonify(users=IPContainer.getNumberOfUsers())
 
 @app.route("/getNumberOfNetworks", methods=['GET'])
 def getNumberOfNetworks():
-    return jsonify(networks=ipc.getNumberOfNetworks())
+    return jsonify(networks=IPContainer.getNumberOfNetworks())
 
 @app.route("/existNetwork/<user>/<_type>")
 def existNetwork(user, _type):
-    if ipc.existNetwork(str(user), str(_type)):
-        return jsonify(exists=True)
-    else:
-        return jsonify(exists=False)
+    return jsonify(exists=IPContainer.existNetwork(str(user), str(_type)))
 
 @app.route("/createNetwork/<user>/<_type>")
 def createNetwork(user, _type):
-    if ipc.createNetwork(str(user), str(_type)):
-        return jsonify(success=True)
-    else:
-        return jsonify(success=False)
+    return jsonify(success=IPContainer.createNetwork(str(user), str(_type)))
 
 @app.route("/removeNetwork/<user>/<_type>")
 def removeNetwork(user, _type):
-    if ipc.removeNetwork(str(user), str(_type)):
-        return jsonify(success=True)
-    else:
-        return jsonify(success=False)
+    return jsonify(success=IPContainer.removeNetwork(str(user), str(_type)))
 
 @app.route("/addIPtoNetwork/<user>/<_type>/<ip>")
 def addIPtoNetwork(user, _type, ip):
-    if ipc.addIPtoNetwork(str(user), str(_type), str(ip)):
-        return jsonify(success=True)
-    else:
-        return jsonify(success=False)
+    return jsonify(success=IPContainer.addIPtoNetwork(str(user), str(_type), str(ip)))
     
-
 @app.route("/removeIPfromNetwork/<user>/<_type>/<ip>")
 def removeIPfromNetwork(user, _type, ip):
-    if ipc.removeIPfromNetwork(str(user), str(_type), str(ip)):
-        return jsonify(success=True)
-    else:
-        return jsonify(success=False)
+    return jsonify(success=IPContainer.removeIPfromNetwork(str(user), str(_type), str(ip)))
 
 @app.route("/getNetworkSize/<user>/<_type>")
 def getNetworkSize(user, _type):
-    return jsonify(user=user, network=_type, size=ipc.getNetworkSize(str(user), str(_type)))
+    return jsonify(user=user, network=_type, size=IPContainer.getNetworkSize(str(user), str(_type)))
 
 @app.route("/getData/<user>/<_type>")
 def getData(user, _type):
-    return jsonify(user=user, network=_type, data=ipc.getData(str(user), str(_type)))
+    return jsonify(user=user, network=_type, data=IPContainer.getData(str(user), str(_type)))
 
 @app.route("/_dropUsers")
 def _dropUsers():
-    if ipc._dropUsers():
-        return jsonify(success=True)
-    else:
-        return jsonify(success=False)
+    IPContainer._dropUsers()
 
 @app.route("/_dropData")
 def _dropData():
-    if ipc._dropData():
-        return jsonify(success=True)
-    else:
-        return jsonify(success=False)
+    IPContainer._dropData()
+
+@app.route("/getUsers")
+def getUsers():
+    return jsonify(users=IPContainer.getUsers())
 
 if __name__ == "__main__":
     app.run()

039ba3efd58cdc7397a14959e3287100d19bc7cb
harvestcore <aagomezies@gmail.com>
2018-11-11 23:26:28 +0100

    Archivo sql definitivo

--- ipcdb.sql
+++ ipcdb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.7.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 26-10-2018 a las 10:14:34
--- Versión del servidor: 8.0.13
--- Versión de PHP: 7.2.10-0ubuntu0.18.04.1
+-- Servidor: sql2.freemysqlhosting.net
+-- Tiempo de generación: 11-11-2018 a las 22:21:08
+-- Versión del servidor: 5.5.54-0ubuntu0.12.04.1
+-- Versión de PHP: 7.0.32-0ubuntu0.16.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `ipcontainerdb`
+-- Base de datos: `ipcdb`
 --
 
 -- --------------------------------------------------------
@@ -29,11 +29,13 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `apiusers` (
-  `id` int(7) NOT NULL,
-  `public_id` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `name` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
-  `password` varchar(100) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  `id` int(11) NOT NULL,
+  `public_id` varchar(50) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `admin` tinyint(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 -- --------------------------------------------------------
 
@@ -42,11 +44,11 @@ CREATE TABLE `apiusers` (
 --
 
 CREATE TABLE `data` (
-  `id` int(7) NOT NULL,
-  `username` varchar(128) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
-  `type` varchar(4) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
-  `data` json NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  `id` int(11) NOT NULL,
+  `username` varchar(128) DEFAULT NULL,
+  `type` varchar(4) DEFAULT NULL,
+  `data` varchar(1000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -55,9 +57,9 @@ CREATE TABLE `data` (
 --
 
 CREATE TABLE `users` (
-  `id` int(7) NOT NULL,
-  `username` varchar(35) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+  `id` int(11) NOT NULL,
+  `username` varchar(128) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Índices para tablas volcadas
@@ -67,8 +69,7 @@ CREATE TABLE `users` (
 -- Indices de la tabla `apiusers`
 --
 ALTER TABLE `apiusers`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `public_id` (`public_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `data`
@@ -80,8 +81,7 @@ ALTER TABLE `data`
 -- Indices de la tabla `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `username` (`username`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -91,20 +91,17 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `apiusers`
 --
 ALTER TABLE `apiusers`
-  MODIFY `id` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 --
 -- AUTO_INCREMENT de la tabla `data`
 --
 ALTER TABLE `data`
-  MODIFY `id` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(7) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
-COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;

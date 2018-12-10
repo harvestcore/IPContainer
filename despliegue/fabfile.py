cc35cde92a38e2595b9b73f34358a6f5713da9b8
harvestcore <aagomezies@gmail.com>
2018-12-10 15:25:47 +0100

    Agrego maquina producción. Cambios menores. Continues #29

--- despliegue/fabfile.py
+++ despliegue/fabfile.py
@@ -2,16 +2,19 @@ from fabric.api import *
 
 env.use_ssh_config = True
 
-def test():
+def staging():
     env.hosts = ['ubuntu']
+
+def production():
+    env.hosts = ['ipcontainer']
 
 def database_up():
     sudo('chown ubuntu:ubuntu /var/run/docker.sock')
-    run('docker run -d --name ipcsql harvestcore/ipcontainer-mysql')
-    run('sleep 20')
+    run('docker run -d -p 3306:3306 --name ipcsql harvestcore/ipcontainer-mysql')
+    run('sleep 30')
 
 def app_up():
-    run('docker run -d -p 80:5000 -e MYSQL_KEY=mysql+mysqlconnector://root:root@172.17.0.2:3306/ipcdb -e "SECRET_KEY=rQsgiA2EupfZTo7WIBY61CmHMWrUTvRBl6JiITvp1GW2uyP5rhHWEh3KZAb3R2F7" --name ipc harvestcore/ipcontainer')
+    run('docker run -d -p 80:5000 -e MYSQL_KEY="mysql+mysqlconnector://root:root@172.17.0.2:3306/ipcdb" -e "SECRET_KEY=rQsgiA2EupfZTo7WIBY61CmHMWrUTvRBl6JiITvp1GW2uyP5rhHWEh3KZAb3R2F7" --name ipc harvestcore/ipcontainer')
 
 def database_down():
     run('docker stop ipcsql')

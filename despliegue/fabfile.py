45af93e022ceccf2a3dc9a1ce51d228610a8fc30
harvestcore <aagomezies@gmail.com>
2018-12-14 11:23:36 +0100

    Quito comandos para arrancar mysql. Continues #29

--- despliegue/fabfile.py
+++ despliegue/fabfile.py
@@ -8,16 +8,8 @@ def staging():
 def production():
     env.hosts = ['ipcontainer']
 
-def database_up():
-    sudo('chown ubuntu:ubuntu /var/run/docker.sock')
-    run('docker run -d -p 3306:3306 --name ipcsql harvestcore/ipcontainer-mysql')
-    run('sleep 30')
-
 def app_up():
-    run('docker run -d -p 80:5000 -e MYSQL_KEY="mysql+mysqlconnector://root:root@172.17.0.2:3306/ipcdb" -e "SECRET_KEY=rQsgiA2EupfZTo7WIBY61CmHMWrUTvRBl6JiITvp1GW2uyP5rhHWEh3KZAb3R2F7" --name ipc harvestcore/ipcontainer')
-
-def database_down():
-    run('docker stop ipcsql')
+    run('docker run -d -p 80:5000 --env-file ~/env.list --name ipc harvestcore/ipcontainer')
 
 def app_down():
     run('docker stop ipc')
@@ -31,17 +23,12 @@ def dockerimages():
 def dockerps():
     run('docker ps')
 
-def update_db():
-    run('docker pull harvestcore/ipcontainer-mysql')
-
 def update_app():
     run('docker pull harvestcore/ipcontainer')
 
-def ipcontainer_up():
+def ipc_up():
     execute(dockerprune)
-    execute(database_up)
     execute(app_up)
 
-def ipcontainer_down():
-    execute(database_down)
+def ipc_down():
     execute(app_down)
